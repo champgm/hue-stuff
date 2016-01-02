eb4dc127ba3393a0c51f2b797f7135fd6afbe74e
Mac Champion <mac.champion@cerner.com>
2016-01-02 09:44:43 -0600

    ok

--- hue-web/src/com/cgm/java/hue/web/GetLights.java
+++ hue-web/src/com/cgm/java/hue/web/GetLights.java
@@ -1,6 +1,8 @@
 package com.cgm.java.hue.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,13 +13,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class HueServlet
  */
 @WebServlet("/HueServlet")
-public class HueServlet extends HttpServlet {
+public class GetLights extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * {@link HttpServlet#HttpServlet()}
      */
-    public HueServlet() {
+    public GetLights() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,8 +28,13 @@ public class HueServlet extends HttpServlet {
      * {@link HttpServlet#doGet(HttpServletRequest, HttpServletResponse)}
      */
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        final PrintWriter writer = response.getWriter();
+//        writer.append("Served at: ").append(request.getContextPath());
+//        writer.append("\n")
+//        writer.append("Blah blah blah blah blah.");
+
+        
+
     }
 
     /**
