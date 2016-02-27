f1a6785296f43cde946a7f1b0180e46a30705f85
Na Zhao <na.zhao@cerner.com>
2016-02-27 13:19:18 -0600

    modified format and used bootstrap grid to enhance the page

--- hue-web/src/main/webapp/bootstrap/js/buttonEvents.js
+++ hue-web/src/main/webapp/bootstrap/js/buttonEvents.js
@@ -1,20 +1,19 @@
-$(document).ready(function() {	
-		
-	$('.light').on('change', function () {
-//        if ($(this).prop('checked')) {
-           // do what needed here   
-           var lightId = this.id;
-           $.get('togglelight', {'lightid': lightId},
-        		   function(resp) {
-        	   
-           }
-           )
-           .fail (function() {
-        	   alert('request failed!');
-           })
-//        }
-//        else {
-           // do what needed here
-//        }
-      });
+$(document).ready(function() {
+
+	$('.light').on('change', function() {
+		// if ($(this).prop('checked')) {
+		// do what needed here
+		var lightId = this.id;
+		$.get('togglelight', {
+			'lightid' : lightId
+		}, function(resp) {
+
+		}).fail(function() {
+			alert('request failed!');
+		})
+		// }
+		// else {
+		// do what needed here
+		// }
+	});
 });
