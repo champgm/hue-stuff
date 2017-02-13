dc470b12141002d6944f7323261c19417d4c3cf5
Mac Champion <mac.champion@careerbuilder.com>
2017-02-13 16:08:29 -0500

    test commit, please fixup

--- quickstart/src/app/app.module.ts
+++ quickstart/src/app/app.module.ts
@@ -1,13 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero/hero-detail.component';
+import { HeroesComponent } from './hero/heroes.component';
+import { HeroService } from './hero/hero.service';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeroDetailComponent],
-  bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent],
+  bootstrap: [AppComponent],
+  providers: [HeroService]
 })
+
 export class AppModule { }
