23fbbe7fa37b2175edccd804a9aaf765076bd9b1
Mac Champion <mac.champion@careerbuilder.com>
2017-02-13 14:30:36 -0500

    test commit, please fixup

--- quickstart/src/app/app.module.ts
+++ quickstart/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero/hero-detail.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HeroDetailComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
